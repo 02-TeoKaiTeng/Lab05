db2a0ea7730aaeb6901fcf893c625f9cfd43ee84
02-TeoKaiTeng <91868652+02-TeoKaiTeng@users.noreply.github.com>
2021-10-12 20:13:45 +0700

    Created Player Script and Test Play

---
+++ Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player : MonoBehaviour
+{
+    public Text ScoreText;
+    private float scoreValue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coin")
+        {
+            scoreValue += 10;
+            ScoreText.text = "Score: " + scoreValue;
+            Destroy(other.gameObject);
+        }
+        if(other.gameObject.tag == "Water")
+        {
+            SceneManager.LoadScene("GameLoseScene");
+            GameObject.Find("FPSController").GetComponent<FirstPersonController>().m_MouseLook.m_cursorIsLocked = false;
+        }
+    }
+}
